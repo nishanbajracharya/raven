ed3e613d025a756fcacf1021098ff2830c6ab6cb
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2020-11-14 15:05:50 +0545

    Add message styling

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,36 @@
 import Head from 'next/head';
+import Linkify from 'react-linkify';
 import { io } from 'socket.io-client';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { Container, Input, HStack, IconButton } from '@chakra-ui/react';
 
 import { getMessages, sendMessage } from '../services/message';
 
+import getUser from '../utils/user';
+import styles from './message.module.css';
+
+function Message(props) {
+  return (
+    <div
+      className={`${styles.message} ${
+        props.data.user === getUser() ? styles['message-user'] : ''
+      }`}
+    >
+      <Linkify
+        componentDecorator={(href, text, key) => (
+          <a href={href} key={key} target="_blank" rel="noopener noreferrer">
+            {text}
+          </a>
+        )}
+      >
+        {props.data.text}
+      </Linkify>
+    </div>
+  );
+}
+
 export default function Home() {
+  const list = useRef(null);
   const [messages, setMessages] = useState([]);
 
   function fetchMessages() {
@@ -19,33 +45,59 @@ export default function Home() {
     const socket = io();
 
     socket.on('message', (data) => {
-      setMessages([...messages, data]);
+      setMessages((prevMessages) => [...prevMessages, data]);
       fetchMessages();
     });
+
+    if (list) {
+      list.current.addEventListener('DOMNodeInserted', (event) => {
+        const target = event.currentTarget;
+
+        target.scroll({ top: target.scrollHeight, behavior: 'smooth' });
+      });
+    }
   }, []);
 
   function handleSubmit(e) {
     e.preventDefault();
     const formData = new FormData(e.target);
-
     e.target.message.value = '';
 
-    sendMessage(formData.get('message'));
+    let message = formData.get('message');
+
+    message = message && message.trim();
+
+    if (!message) return;
+
+    sendMessage(message);
   }
 
   return (
-    <div>
+    <Container maxW="xl">
       <Head>
         <title>Raven</title>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        />
       </Head>
-      {messages.map((message) => (
-        <p key={message.id}>{message.text}</p>
-      ))}
-      <form onSubmit={handleSubmit}>
-        <input type="text" id="message" name="message" />
-        <button type="submit">Send</button>
+      <div className={styles.list} ref={list}>
+        {messages.map((message) => (
+          <Message data={message} key={message.id} />
+        ))}
+      </div>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <HStack>
+          <Input type="text" id="message" name="message" autoComplete="off" />
+
+          <IconButton
+            type="submit"
+            colorScheme="blue"
+            icon={<span className="material-icons">send</span>}
+          />
+        </HStack>
       </form>
-    </div>
+    </Container>
   );
 }
